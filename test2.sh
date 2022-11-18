9d264a4d019197762cebe6f5e17bce0fded2a453
Eric <pante.eric@gmail.com>
2022-11-18 17:27:21 +0100

    Delete test2.sh

--- test2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# USAGE:
-# parsing_vcf.sh FILE.vcf
-
-total.n.alleles=$(cat toy.vcf | sed  '/^#/d' | tr -s " " | cut -d " " -f 5 | sed 's/,//g' | awk '{print length +1}')
-echo '$total.n.alleles'
